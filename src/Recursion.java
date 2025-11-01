6077b2a3fb68a228b98a9acc2e51c196fcd4c547
utk4rshgupta <utkarshgupta2803@gmail.com>
2025-11-01 23:49:28 +0530

    Formula

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,15 +1,17 @@
-
-
 public class Recursion {
     public static void main() {
-        System.out.println(fibo(30));
+        System.out.println(fiboFormula(30));
     }
+
     static int fibo(int n ){
         if(n<2){
             return n;
         }
         else
             return fibo(n-1)+fibo(n-2);
+    }
+    static  int fiboFormula(int n){
+        return (int)(Math.pow(((1+Math.sqrt(5) )/2 ),n )/ Math.sqrt(5));
     }
     }
 
