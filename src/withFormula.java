f6edf1f9a8c23236e6b71430bdd09dc147108fc6
utk4rshgupta <utkarshgupta2803@gmail.com>
2025-11-02 23:32:27 +0530

    Mathematical  Formula of Recursion

---
+++ src/withFormula.java
@@ -0,0 +1,8 @@
+public class withFormula {
+    static  int fiboFormula(int n){
+        return (int)(Math.pow(((1+Math.sqrt(5) )/2 ),n )/ Math.sqrt(5));
+    }
+    public static void main() {
+        System.out.println(fiboFormula(30));
+    }
+}
