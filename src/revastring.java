462815b66326e1a672d16b198823f612d073f1a9
utk4rshgupta <utkarshgupta2803@gmail.com>
2025-11-20 22:55:05 +0530

    reversing string

---
+++ src/revastring.java
@@ -0,0 +1,14 @@
+public class revastring {
+    public static void main(String[] args) {
+
+    }
+    public static void rev(String s){
+        if(s.length()==0){
+            return;
+        }
+        int x=0;
+        System.out.println(s.charAt(x));
+        x++;
+
+    }
+}
