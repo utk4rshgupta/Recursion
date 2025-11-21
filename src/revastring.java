2f43ab284fc2762d1fc1997aa5cdc454872977a1
utk4rshgupta <utkarshgupta2803@gmail.com>
2025-11-21 23:56:14 +0530

    reversing string

--- src/revastring.java
+++ src/revastring.java
@@ -1,14 +1,14 @@
 public class revastring {
     public static void main(String[] args) {
-
+    String str = "utkarsh";
+     rev(str);
     }
     public static void rev(String s){
-        if(s.length()==0){
+        if(s.equals("")){
             return;
         }
-        int x=0;
-        System.out.println(s.charAt(x));
-        x++;
+        rev(s.substring(1));
+        System.out.print(s.charAt(0));
 
     }
 }
