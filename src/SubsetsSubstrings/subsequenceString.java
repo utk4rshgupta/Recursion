644f706e8b22820c3fad6108c13d5909dc9e03ba
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-01-24 21:56:44 +0530

    qns

---
+++ src/SubsetsSubstrings/subsequenceString.java
@@ -0,0 +1,41 @@
+package SubsetsSubstrings;
+
+import java.util.ArrayList;
+
+public class subsequenceString {
+    //using recursion
+
+    public static void main(String[] args) {
+        //System.out.println(subseq1("","abc"));
+    }
+//    static void subseq(String p,String up){
+//        if(up.isEmpty()){
+//            System.out.println(p);
+//            return;
+//        }
+//        char ch = up.charAt(0);
+//        subseq(p,up.substring(1));
+//        subseq(p+ch,up.substring(1));
+//    }
+
+    // add it in a arrayList
+
+//    static ArrayList<String> subseq1(String p,String up){
+//        if(up.isEmpty()){
+//            ArrayList<String> list = new ArrayList<>();
+//            list.add(p);
+//            return list;
+//        }
+//        char ch = up.charAt(0);
+//        ArrayList<String> right = subseq1(p,up.substring(1));
+//        ArrayList<String> left = subseq1(p+ch,up.substring(1));
+//        left.addAll(right);
+//        return left;
+//    }
+
+   // static ArrayList<Integer> list = new ArrayList<>();
+
+
+
+
+}
--- src/nto1.java
+++ src/nto1.java
@@ -1,12 +1,15 @@
 public class nto1 {
-    public static void main() {
-        print(5);
+
+    public static void main(String args[]){
+        print(500);
     }
     public static void print(int n){
         if(n==0){
             return;
         }
+
         System.out.println(n);
         print(n-1);
+
     }
 }
