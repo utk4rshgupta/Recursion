21227981abf4d8c2f594756b379fa4d9337c353f
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-01-26 21:22:28 +0530

    permutations

--- src/SubsetsSubstrings/permutations.java
+++ src/SubsetsSubstrings/permutations.java
@@ -2,17 +2,22 @@ package SubsetsSubstrings;
 
 public class permutations {
     public static void main(String[] args){
-
+    permutations("","abc");
+        System.out.println(count);
     }
+    static int count =0;
     static void permutations(String p,String up){
         if(up.isEmpty()){
             System.out.println(p);
             return;
         }
         char ch = up.charAt(0);
-        for (int i = 0; i < p.length(); i++) {
+        for (int i = 0; i <= p.length(); i++){
+            String s = p.substring(i,p.length());
             String f = p.substring(0,i);
-            String s= p.substring(i,p.length());
+
+            permutations(f+ch+s,up.substring(1));
+            count++;
 
         }
     }
