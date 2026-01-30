fd10fdf5e1f587f70275e9bbf4d88b4045876388
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-01-30 21:40:06 +0530

    dice problem

---
+++ src/SubsetsSubstrings/diceCombination.java
@@ -0,0 +1,35 @@
+package SubsetsSubstrings;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class diceCombination {
+public static void main(String[] args) {
+    //dice("",4);
+    ArrayList<String> ans = diceList("",4);
+    System.out.println(ans);
+}
+
+    static void dice(String p , int target){
+        if (target == 0) {
+            System.out.println(p);
+            return;
+        }
+        for(int i =1;i<=6 && i<=target;i++){
+            dice(p+i,target-i);
+        }
+    }
+
+    static ArrayList<String> diceList(String p , int target){
+        if (target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for(int i =1;i<=6 && i<=target;i++){
+            list.addAll(diceList(p+i,target-i));
+        }
+        return list;
+    }
+}
